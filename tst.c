7ddc3c707ac5e35ff9fa84eaba6fd763d164c9e6
Youness Rziqi <younessrziki204@gmail.com>
2026-03-29 19:22:49 +0100

    Update

--- tst.c
+++ tst.c
@@ -1,23 +1,47 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 
-void *func()
+const int counter = 1000000;
+int var = 0;
+bool is_finiche = false;
+
+void *inc()
 {
-    printf("Hello Wordl\n");
+    for (int i = 0; i < counter; i++)
+    {
+        var++;
+    }
+
+    is_finiche = true;
     return NULL;
 }
 
+void *dec()
+{
+    while (!is_finiche);
+
+    for (int i = 0; i < counter; i++)
+    {
+        var--;
+    }
+
+    return NULL;
+}
 
 int main(void)
 {
 
-    pthread_t th;
+    pthread_t th[2];
 
+    pthread_create(&th[0], NULL, inc, NULL);
+    pthread_create(&th[1], NULL, dec, NULL);
+    
+    pthread_join(th[0], NULL);
+    pthread_join(th[1], NULL);
 
-    pthread_create(&th, NULL, func, NULL);
-    pthread_join(th, NULL);
+    printf("var = %d\n",var);
 
     return 0;
 }
